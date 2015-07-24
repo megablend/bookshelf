59f36da597b01a55f1f6e4a2c9b9e563087f06e0
Megafu Charles <megafu.charles@yahoo.com>
2015-07-24 11:09:04 +0100

    Added new layout files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	#display page titles
+	def page_title(title)
+       default_title = "Welcome to Africa's best online eBook store"
+       if title.empty? then default_title else title + ' | ' + default_title end
+	end
 end
