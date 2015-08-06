6ede0fdefba370c9cfad10586e0532502fc98b2b
Megafu Charles <megafu.charles@yahoo.com>
2015-08-06 09:00:52 +0100

    Added changes to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+   @user = User.new(user)
+	   if(@user.save)
+	   	# save data
+	   else
+	   	render 'new'
+	   end
+  end
+
+  private
+
+	  def user_params
+	     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
 end
